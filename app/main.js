c68ecb19d27da09e7dd0a517b5ca6f0b7fa0f54c
kristoffach <kristofferfh.kodehode@gmail.com>
2024-03-25 20:26:08 +0100

    main application entry point

--- app/main.js
+++ app/main.js
@@ -1,22 +1,30 @@
 // Import dependencies
 import express from 'express'
-import path from 'path'
-import discord from './services/discord/client.js'
+import cors from "cors"
+import discordBot from './services/discord/client.js'
+import connectDB from './services/mongodb/connect.js'
 
-const port = 2000
+// Import configs from .env
+import { EXPRESS_PORT, MONGODB_URL } from "./config/index.js"
 
-// Initilize app
+// Initilize express app
 const app = express()
 
+// Initilize dependencies
+app.use(cors())
+app.use(express.json())
+
+// Connect to mongoDB
+connectDB(MONGODB_URL)
+
+// Start discord.js bot
+try { discordBot() }
+catch (err) { console.log('An error occoured starting the discord bot, read logs =>\n', err) }
+
+// Temporary html response page
 app.get('/', (request, response) => {
 	return response.sendFile('app/pages/index.html', { root: '.' })
 })
 
-app.listen(port, () => console.log(`Express app listening at http://localhost:${port}`))
-
-
-// Start discord bot
-// discord()
-
-// Initilize dependencies
-
+// Listen for requests
+app.listen(EXPRESS_PORT, () => console.log(`Express app listening at http://localhost:${EXPRESS_PORT}`))
