b64568cfe78b335f26a57643911a151f6576695b
kristoffach <kristofferfh.kodehode@gmail.com>
2024-04-02 13:07:40 +0200

    Fixed date issue with miliseconds format

--- client/src/views/attendance/Create.vue
+++ client/src/views/attendance/Create.vue
@@ -1,10 +1,9 @@
 <script setup>
+  import { ref, watch } from "vue"
   import Calendar from 'primevue/calendar'
   import FloatLabel from 'primevue/floatlabel'
   import InputNumber from 'primevue/inputnumber'
   import Button from 'primevue/button'
-
-  import { ref, watch } from "vue"
 
   const date = ref()
   const offset = ref()
@@ -23,11 +22,10 @@
   const submit = async () => {
     if (offset.value < 1 || offset.value === undefined) offset.value = 1
     const from = date.value.getTime()
-    const to = date.value.getTime() + ( offset.value * 60 * 60 )
-
+    const to = date.value.getTime() + ( offset.value * 60 * 60 * 1000 )
 
     try {
-      const body = {"from":from, "to":to}
+      const body = { "from": from, "to": to }
       const request = { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(body) }
       const response = await fetch('http://localhost:2000/post/attendance/testdata', request)
       const data = await response.json()
