1440de34ac0df2f6e3da06c0ce2ba8f041cc2a78
kristoffach <kristofferfh.kodehode@gmail.com>
2024-04-02 10:24:16 +0200

    Adding cors options

--- server/main.js
+++ server/main.js
@@ -20,21 +20,24 @@ export const __dirname = path.dirname(__filename)
 const server = express()
 const client = express()
 
+// Setup cors
+// const corsOptions = { origin: "http://localhost:3000" }
+
 // Initilize dependencies
-server.use(cors())
+server.use(cors({ origin: "http://localhost:3000" }))
 server.use(express.json())
 
 // Connect to mongoDB with mongoose
-// connectDB(MONGODB_URL + MONGODB_DB)
+connectDB(MONGODB_URL + MONGODB_DB)
 
 // Start discord.js bot
 // connectDiscordBot()
 
 // Define static path for client assets
-client.use(express.static(path.join(__dirname, '../client')))
+client.use(express.static(path.join(__dirname, '../client/dist')))
 
 // Handle HTML response
-client.get('*', ( req, res ) => { res.sendFile(path.join(__dirname, '../client/index.html'), { root: '.' }) })
+client.get('/', ( req, res ) => { res.sendFile(path.join(__dirname, '../client/dist/index.html')) })
 
 // Handle api requests
 server.use('/', apiRouter)
