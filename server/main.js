c6f704be2c69361d070589d6c1db2711c79078c7
kristoffach <kristofferfh.kodehode@gmail.com>
2024-04-02 09:03:47 +0200

    Seperated client and server functions

--- server/main.js
+++ server/main.js
@@ -1,11 +1,21 @@
 // Import dependencies
 import express from 'express'
 import cors from "cors"
-import discordBot from './services/discord/client.js'
+import connectDiscordBot from './services/discord/client.js'
 import connectDB from './services/mongodb/connect.js'
 
+// Import routes
+import attendanceRouter from './routes/attendance.js'
+import apiRouter from './routes/api.js'
+
 // Import configs from .env
-import { EXPRESS_PORT, MONGODB_URL } from "./config/index.js"
+import { EXPRESS_CLIENT_PORT, EXPRESS_SERVER_PORT, MONGODB_URL, MONGODB_DB } from "./config/index.js"
+
+// ES6 import fix for path
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // Initilize express servers
 const server = express()
@@ -15,14 +25,22 @@ const client = express()
 server.use(cors())
 server.use(express.json())
 
-// Connect to mongoDB
-connectDB(MONGODB_URL)
+// Connect to mongoDB with mongoose
+// connectDB(MONGODB_URL + MONGODB_DB)
 
 // Start discord.js bot
-try { discordBot() }
-catch (err) { console.log('An error occoured starting the discord bot, read logs =>\n', err) }
+// connectDiscordBot()
 
+// Define static path for client assets
+client.use(express.static(path.join(__dirname, '../client')))
 
+// Handle HTML response
+client.get('/', ( req, res ) => { res.sendFile(path.join(__dirname, '../client/index.html'), { root: '.' }) })
+client.get('/users', ( req, res ) => { res.sendFile(path.join(__dirname, '../client/pages/users/index.html')) })
+client.use('/attendance', attendanceRouter)
+
+// Handle api requests
+server.use('/api', apiRouter)
 
 
 // Temporary html response page
@@ -31,5 +49,5 @@ server.get('/', (request, response) => {
 })
 
 // Listen for requests
-server.listen(EXPRESS_PORT, () => console.log(`Backend server listening at http://localhost:${EXPRESS_PORT}`))
-client.listen(EXPRESS_PORT, () => console.log(`Frontend server listening at http://localhost:${EXPRESS_PORT}`))
+server.listen(EXPRESS_SERVER_PORT, () => console.log(`Backend server listening at http://localhost:${EXPRESS_SERVER_PORT}`))
+client.listen(EXPRESS_CLIENT_PORT, () => console.log(`Frontend server listening at http://localhost:${EXPRESS_CLIENT_PORT}`))
