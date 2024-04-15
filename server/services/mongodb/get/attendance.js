8f7574790c7fcf2ac863528ab63db60567cc130c
kristoffach <kristofferfh.kodehode@gmail.com>
2024-04-15 16:40:17 +0200

    Get requests for attendance will now include user ref aswell

--- server/services/mongodb/get/attendance.js
+++ server/services/mongodb/get/attendance.js
@@ -3,13 +3,13 @@ import { AttendanceModel } from "../../../models/attendance.js"
 const getAttendance = async (params) => {
   // No date was supplied, looking up all entries in attendance
   if (Object.keys(params).length === 0) {
-    return await AttendanceModel.find().populate('messages').exec()
+    return await AttendanceModel.find().populate({ path: 'messages', populate: { path: 'user', model: 'User' }}).exec()
   }
 
   // Date range supplied, looking for entries between dates
   if (params.from && params.to) {
     const date = { $gte: new Date(params.from), $lt: new Date(params.to) }
-    return await AttendanceModel.find({date:date}).populate('messages').exec()
+    return await AttendanceModel.find({date:date}).populate({ path: 'messages', populate: { path: 'user', model: 'User' }}).exec()
   }
 
   // Looking up specific day
@@ -21,13 +21,8 @@ const getAttendance = async (params) => {
     to.setSeconds(59)
 
     const date = { $gte: new Date(params.from), $lt: new Date(to) }
-    return await AttendanceModel.find({date:date}).populate('messages').exec()
+    return await AttendanceModel.find({date:date}).populate({ path: 'messages', populate: { path: 'user', model: 'User' }}).exec()
   }
-
-
-
-  // Search db for matching params and return array of results
-  // return await AttendanceModel.findOne(params).populate('messages').exec()
 }
 
 export default getAttendance
